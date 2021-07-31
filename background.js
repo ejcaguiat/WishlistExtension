ed4fc9b8aca6b6025f18356b5ed59f40cfbe520d
Emmil Joshua Caguiat <ejcaguiat@Joshs-MBP.fios-router.home>
2021-07-31 18:55:32 -0400

    First commit

---
+++ background.js
@@ -0,0 +1,9 @@
+//functionality for the extension
+
+let color = '#3aa757';
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.sync.set({color});
+    console.log('Default background color set to %cgreen', 'color: ${color}');
+
+});
